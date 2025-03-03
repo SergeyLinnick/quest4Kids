60508613edd76769373adfd69e2f20257aa4b76a
tatianakuzenkova <tatiana.kuzenkova@gentlecode.com>
2025-03-03 14:50:27 +0100

    Q4K: Add cache

--- packages/api/src/children/actions.ts
+++ packages/api/src/children/actions.ts
@@ -1,25 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 
-import { unstable_cache } from "next/cache";
+// import { unstable_cache } from "next/cache";
 import { cookies } from "next/headers";
 import { getChildrenURL } from "./services";
 
-const getData = unstable_cache(
-  async (token) => {
-    console.log("Fetching fresh data...");
-    const res = await fetch(getChildrenURL, {
-      headers: token ? { Authorization: `Bearer ${token}` } : {},
-      cache: "force-cache",
-    });
-    return res.json();
-  },
-  ["children-list"],
-  { revalidate: 60, tags: ["children-list"] },
-);
+// const getData = unstable_cache(
+//   async (token) => {
+//     console.log("Fetching fresh data...");
+//     const res = await fetch(getChildrenURL, {
+//       headers: token ? { Authorization: `Bearer ${token}` } : {},
+//     });
+//     return res.json();
+//   },
+//   ["children-list"],
+//   { revalidate: 60, tags: ["children-list"] },
+// );
+
+const getData = async (token: string | null) => {
+  console.log("Fetching fresh data...");
+  const res = await fetch(getChildrenURL, {
+    headers: token ? { Authorization: `Bearer ${token}` } : {},
+    cache: "force-cache",
+    next: { tags: ["children-list"], revalidate: 60 },
+  } as any);
+
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`);
+  }
+
+  return res.json();
+};
 
 export async function fetchChildren() {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value || null;
   const children = await getData(token);
+
   return children?.data;
 }

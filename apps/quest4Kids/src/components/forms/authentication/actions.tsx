6cc11416520d9ac84e29610a0831e32dab73d23a
Serhii <sergey.linnick@gentlecode.com>
2025-03-13 14:21:09 +0100

    Q4K: Replace static url with process.env

--- apps/quest4Kids/src/components/forms/authentication/actions.tsx
+++ apps/quest4Kids/src/components/forms/authentication/actions.tsx
@@ -4,6 +4,8 @@ import { PAGE_PATH } from "@/consts";
 import { signIn } from "@repo/auth";
 import { redirect } from "next/navigation";
 
+const api = process.env.NEXT_PUBLIC_API_URL;
+
 type Error = Map<string, string>;
 
 type FormState = {
@@ -25,16 +27,13 @@ export async function registerUser(
   const name = formData.get("name") as string;
 
   try {
-    const response = await fetch(
-      "https://quest4kids-a7fd24f91954.herokuapp.com/auth/register",
-      {
-        method: "POST",
-        body: JSON.stringify({ email, password, name }),
-        headers: {
-          "Content-Type": "application/json",
-        },
+    const response = await fetch(`${api}/auth/register`, {
+      method: "POST",
+      body: JSON.stringify({ email, password, name }),
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+    });
 
     if (!response.ok) {
       const errorData = await response.json();
@@ -59,39 +58,4 @@ export async function loginUser(
   formData: FormData,
 ): Promise<FormState> {
   return await signIn("credentials", formData);
-
-  // try {
-  //   const result: { ok: boolean; error?: string } = await signIn(
-  //     "credentials",
-  //     formData,
-  //   );
-
-  //   console.log("result -----------------------", result);
-
-  //   if (!result?.ok) {
-  //     const errorMessage = result?.error || "Invalid credentials!";
-  //     console.error("Login error:", errorMessage, { email });
-  //     return {
-  //       errors: new Map([["form", errorMessage]]),
-  //       values: { email },
-  //     };
-  //   }
-
-  //   return {
-  //     errors: new Map(),
-  //     success: true,
-  //     values: {},
-  //   };
-  // } catch (error) {
-  //   const errorMessage =
-  //     error instanceof Error
-  //       ? error.message
-  //       : "An unknown error occurred during login!";
-  //   console.error("Login error:", errorMessage, { email });
-
-  //   return {
-  //     errors: new Map([["form", errorMessage]]),
-  //     values: { email },
-  //   };
-  // }
 }
